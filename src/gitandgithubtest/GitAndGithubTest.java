a4491fc7589b94f0491536c121fa1d3fd69a3f5a
carlo <carlo@Karlos>
2024-01-11 15:55:42 +0100

    Inicialización de mi nueva aplicación

---
+++ src/gitandgithubtest/GitAndGithubTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitandgithubtest;
+
+/**
+ *
+ * @author carlo
+ */
+public class GitAndGithubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("esta linea de código viene de local");
+    }
+    
+}
